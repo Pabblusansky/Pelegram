91727595ca43ff42bcc49dace5e39148c31ddc4b
Pavel <pchirtoca@gmail.com>
2025-06-12 11:33:20 +0300

    fix: fix typos

--- server/models/Chat.js
+++ server/models/Chat.js
@@ -4,7 +4,7 @@ const chatSchema = new mongoose.Schema({
   participants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
   messages: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Message' }],
   lastMessage: { type: mongoose.Schema.Types.ObjectId, ref: 'Message' },
-    pinnedMessage: { type: mongoose.Schema.Types.ObjectId, ref: 'Message', default: null }, // <--- НОВОЕ ПОЛЕ
+  pinnedMessage: { type: mongoose.Schema.Types.ObjectId, ref: 'Message', default: null },
   updatedAt: { type: Date, default: Date.now },
 });
 
